85eef9bd786019ea9d5db51a95b90acc3bc4134c
arisafrianto2304 <arisafrianto23@gmail.com>
2024-07-18 11:50:31 +0700

    golang(gobe) baru bisa koneksi ke postgres(dalam docker) hostnya di ganti (nama container postgress) lalu untuk port gunakan port defaultnya postgres yaitu 5432

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 #### untuk masuk postgres ketika env development --> docker exec -it db psql -U useraris -d dbaris 
 
 #### di golangnya secara manual sediain dari awal swagger fiber sudah bisa dari lokal , lalu docker file di sesuaikan
+#### di golang pastikan bahwa untuk koneksi ke database postgres yaitu Host di isi nama Service database container (db) sedangkan portnya tetap default bawaan postgress yaitu (5432)
--- golang/configs/database.go
+++ golang/configs/database.go
@@ -1,21 +1,51 @@
 package configs
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	_ "github.com/lib/pq"
 )
 
-func ConnectDB() *gorm.DB {
-	dsn := fmt.Sprintf("host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
-		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+func ConnectDB() *sql.DB {
+	host := "db"
+	port := "5432"
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	log.Printf("Connecting to database with the following details:\n")
+	log.Printf("Host: %s\n", host)
+	log.Printf("Port: %s\n", port)
+	log.Printf("User: %s\n", user)
+	log.Printf("Password: %s\n", password) // Hati-hati dalam menampilkan password di log pada produksi
+	log.Printf("Database Name: %s\n", dbname)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	var db *sql.DB
+	var err error
+
+	for i := 0; i < 10; i++ {
+		db, err = sql.Open("postgres", psqlInfo)
+		if err == nil {
+			err = db.Ping()
+		}
+		if err == nil {
+			break
+		}
+		log.Println("Failed to connect to database. Retrying in 5 seconds...")
+		time.Sleep(5 * time.Second)
+	}
+
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
+	log.Println("Successfully connected to database")
 	return db
 }
